f62f374f806316ea8492c00dc49d73f2a32dbc9b
MeekoDev <DiegoCru1024@hotmail.com>
2023-06-12 12:48:07 -0500

    Versión 0.2

--- cliente/src/components/registerPage.js
+++ cliente/src/components/registerPage.js
@@ -13,7 +13,6 @@ export default function RegisterPage() {
         email: '',
         password: ''
     })
-
     const detectarCambio = (event) => {
         const {name, value} = event.target
         setData(prevState => ({
